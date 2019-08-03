6603f73ed981a3bf47ec9e1a28f4ab94d43c5a3b
chripinepet <chripinepeter@gmail.com>
2019-08-03 10:37:41 +0300

    Some changes

--- app.py
+++ app.py
@@ -2,11 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = [
+    {
+    "title": "Python",
+    "bio": "Python is a dynamically interpreted language"
+    },
+    {
+    "title": "Java",
+    "bio": "Java is an object oriented language language"
+    },
+    {
+    "title": "HTML",
+    "bio": "A markup language used for creating webpages"
+    },
+    {
+    "title": "C",
+    "bio": "Used for creating games"
+    },
+    {
+    "title": "CSS",
+    "bio": "Used for styling webpages"
+    }
+    ]
 
 @app.route('/')
 def hello_world():
     title = "Flask Blog"
     return render_template('index.html', title=title)
+
+
+@app.route('/lang/<name>')
+def lang(name):
+    title = name
+    lang = next(item for item in data if item["title"].lower() == name.lower())
+    return render_template('lang.html', title=title, lang=lang)
 
 @app.route('/about')
 def about():
